241de0b6b26ead7dcd109e4971a832c5ab05674f
John Rodney <jrodney@poeticsystems.com>
2015-05-17 03:59:07 -0500

    narrow down the range of the geolocation verification

--- client/quests/quests.js
+++ client/quests/quests.js
@@ -3,7 +3,10 @@ Template.quests.events({
     navigator.geolocation.getCurrentPosition(function(pos){
       var lat = pos.coords.latitude;
       var lon = pos.coords.longitude;
-      if(Math.abs(29.752002 - lat) < .001 && Math.abs(-95.37551 - lon) < .001){
+      var latdiff = Math.abs(29.752002 - lat);
+      var londiff = Math.abs(-95.37551 - lon);
+      console.log(latdiff, londiff);
+      if(latdiff < .0005 && londiff < .0005){
         event.target.innerHTML = "UNLOCKED";
         event.target.classList.remove('unlock');
         event.target.classList.add('unlocked');
